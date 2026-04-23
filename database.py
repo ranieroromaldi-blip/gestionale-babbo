d00a8f099679a923d8138bf56071cc4d4e291ea6
ranieroromaldi-blip <raniero.romaldi@gmail.com>
2026-04-24 00:34:31 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 import sqlite3
+import hashlib
 
 def get_connection():
     return sqlite3.connect("gestionale.db", check_same_thread=False)
-
 
 def init_db():
     conn = get_connection()
     c = conn.cursor()
 
+    # CLIENTI
     c.execute("""
     CREATE TABLE IF NOT EXISTS clienti (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,15 +18,32 @@ def init_db():
     )
     """)
 
+    # INTERVENTI CON SOLDI VERI
     c.execute("""
     CREATE TABLE IF NOT EXISTS interventi (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         cliente TEXT,
         descrizione TEXT,
         data TEXT,
-        stato TEXT
+        stato TEXT,
+        manodopera REAL,
+        materiale REAL,
+        totale REAL
+    )
+    """)
+
+    # UTENTI LOGIN
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS utenti (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT
     )
     """)
 
     conn.commit()
     conn.close()
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
